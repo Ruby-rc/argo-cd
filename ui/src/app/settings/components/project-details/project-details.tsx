400590773601789a87875f06ee8bcd7e69c680c1
ruichao.hu <ruichao.hu@daocloud.io>
2024-08-30 16:57:41 +0800

    fix(ui): add null check for appsInAnyNamespaceEnabled in project-details
    Signed-off-by: ruichao.hu <ruichao.hu@daocloud.io>

--- ui/src/app/settings/components/project-details/project-details.tsx
+++ ui/src/app/settings/components/project-details/project-details.tsx
@@ -661,7 +661,7 @@ export class ProjectDetails extends React.Component<RouteComponentProps<{name: s
                 />
                 <AuthSettingsCtx.Consumer>
                     {authCtx =>
-                        authCtx.appsInAnyNamespaceEnabled && (
+                        authCtx?.appsInAnyNamespaceEnabled && (
                             <EditablePanel
                                 save={item => this.saveProject(item)}
                                 values={proj}
